2e04b5a82c5fc1ce43c0e9aca870d1e7b2835b64
Agustin Alemis <agus.alemis@yahoo.com>
2017-08-14 16:21:53 -0300

    asd
    asdf

---
+++ MyCalculadora/app/src/main/java/com/example/agustin/mycalculadora/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.agustin.mycalculadora;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText num1, num2;
+    Button suma, resta, mult, divi;
+    TextView resultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        num1 = (EditText) findViewById(R.id.num1);
+        num2 = (EditText) findViewById(R.id.num2);
+
+        suma = (Button) findViewById(R.id.sumar);
+        resta= (Button) findViewById(R.id.restar);
+        mult= (Button) findViewById(R.id.mul);
+        divi = (Button) findViewById(R.id.div);
+
+        resultado= (TextView) findViewById(R.id.resultado);
+
+
+        suma.setOnClickListener(this);
+        resta.setOnClickListener(this);
+        mult.setOnClickListener(this);
+        divi.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        String n1 = num1.getText().toString();
+        String n2 = num2.getText().toString();
+
+        int entero1=Integer.parseInt(n1);
+        int entero2=Integer.parseInt(n2);
+
+        int rta=0;
+
+
+        switch (v.getId()){
+            case R.id.sumar:
+                rta=entero1+entero2;
+                break;
+            case R.id.restar:
+                rta=entero1-entero2;
+                break;
+            case R.id.mul:
+                rta=entero1*entero2;
+                break;
+            case R.id.div:
+                if (entero2==0)
+                    Toast.makeText(getApplicationContext(),"No se puede dividir por 0! Por favor ingrese otro numero.",Toast.LENGTH_SHORT).show();
+                else
+                    rta=entero1/entero2;
+                break;
+        }
+
+        resultado.setText(""+rta);
+
+    }
+}
